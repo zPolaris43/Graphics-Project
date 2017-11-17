54d67d22a81a2753ace09ed0aa9302da61afcf63
zPolaris43 <31142634+zPolaris43@users.noreply.github.com>
2017-11-16 22:43:08 -0500

    test
    test

--- Pong.js
+++ Pong.js
@@ -1,6 +1,5 @@
 
 var gl;
-var points;
 
 window.onload = function init() {
     var canvas = document.getElementById("gl-canvas");
@@ -24,7 +23,7 @@ window.onload = function init() {
     //  Configure WebGL
     //
     gl.viewport(0, 0, canvas.width, canvas.height);
-    gl.clearColor(1.0, 1.0, 1.0, 1.0);
+    gl.clearColor(0.0, 0.0, 1.0, 1.0);
 
     //  Load shaders and initialize attribute buffers
 
